63976edc661aed8970d982326c677b3bfe964d11
Harshit Joshi <harsjoshi1614@gmail.com>
2021-10-05 02:15:18 +0530

    lua oop Larget Product !

--- 10001st prime.lua
+++ 10001st prime.lua
@@ -1,6 +1,20 @@
 
 function isPrime(n)
-    for i = 2, n//2 do
+    -- these % filters shreaded another 50 ms !
+    if n%3 == 0 then
+        return false
+    end
+    -- these % filters shreaded another 50 ms !
+    if n%5 == 0 then
+        return false
+    end
+
+    if n%2 == 0 then
+        return false
+    end
+
+    -- not checking even numbers because we already tried 2 (but we already tried 3, 5 too)
+    for i = 3, math.sqrt(n), 2 do
         if n % i == 0 then
             return false
         end
@@ -8,16 +22,38 @@ function isPrime(n)
     return true
 end
 
-primes = {2, 3}
+--[[
+    without isPrime no even opt
+
+    Executed in  230.20 millis    fish           external 
+    usr time  177.60 millis  486.00 micros  177.11 millis 
+    sys time   40.45 millis  198.00 micros   40.25 millis 
+
+    after isPrime no even opt
+
+    Executed in  234.77 millis    fish           external 
+    usr time  108.55 millis    0.00 micros  108.55 millis 
+    sys time   67.69 millis  891.00 micros   66.80 millis 
+
+]]
+
+
+-- hmm i removed the prime numbers allocation/storage in a table
+-- and performace didnot increase a lot.
+
+-- primes = {2, 3}
 
 -- no of iterations => 10000 - 2 + 1 => 9999 (coz lua)
 -- so we already have 2 vals in primes table so + 9999 values = 10001 th value !
+last = 3
 for i = 2, 10000 do
     -- starting from where we left!
-    j=primes[#primes] + 2
+    -- j=primes[#primes] + 2
+    j=last + 2
     while true do
         if isPrime(j) then
-            table.insert(primes, j)
+            -- table.insert(primes, j)
+            last = j
             print("found", j, "\t no: ", i)
             break
         end
@@ -26,4 +62,88 @@ for i = 2, 10000 do
     end
 end
 
-print(primes[#primes])
+print(last)
+-- print(primes[#primes])
+
+
+
+-- result before math.sqrt(n) optimisation in isPrime function
+
+-- ________________________________________________________
+-- Executed in   10.22 secs   fish           external 
+--    usr time   10.09 secs    0.00 micros   10.09 secs 
+--    sys time    0.08 secs  933.00 micros    0.08 secs 
+
+
+-- result after math.sqrt(n) optimisation in isPrime function
+
+-- ________________________________________________________
+-- Executed in  244.40 millis    fish           external 
+--    usr time  198.35 millis    0.00 micros  198.35 millis 
+--    sys time   45.66 millis  1128.00 micros   44.53 millis 
+
+
+-- WOW SUCH DRASTIC CHANGE
+
+
+
+--[[
+check: ->  https://projecteuler.net/thread=7&page=8#last
+
+Barandis: 889823
+
+I wanted to have a lazy sequence of infinitely many prime numbers, and to do so I adapted the Haskell implementation on page 6 of this PDF. 
+[https://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf]
+Once you have the infinite sequence, it's just a matter of looking up the proper index.
+
+# Clojure
+
+(ns barandis.euler.p7)
+
+(def primes
+  ((fn sieve [table x]
+     (let [reinsert (fn [table prime] (update table (+ prime x) conj prime))]
+       (if-let [factors (get table x)]
+         (recur (reduce reinsert (dissoc table x) factors) (inc x))
+         (lazy-seq (cons x (sieve (assoc table (* x x) (list x)) (inc x)))))))
+   {} 2))
+
+(defn- nth-prime [n] (nth primes (dec n)))
+
+(defn solve
+  ([] (solve {}))
+  ([data] (-> (get data :input 10001)
+              nth-prime
+              println
+              time)))
+
+
+# Result
+
+clj꞉barandis.euler.p7꞉> 
+(solve)
+104743
+"Elapsed time: 0.952275 msecs"
+nil
+
+
+Factor already has a lazy list of primes defined, so the solution is trivial.
+
+# Factor
+
+USING: lists math math.primes.lists prettyprint tools.time ;
+IN: barandis.euler.p7
+
+: nth-prime ( n -- p ) 1 - lprimes lnth ;
+: solve ( -- ) [ 10001 nth-prime . ] time ;
+
+MAIN: solve
+
+
+# Result
+
+IN: scratchpad "barandis.euler.p7" run
+104743
+Running time: 0.003164879 seconds
+
+]] 
---
+++ Largest product in a series.lua
@@ -0,0 +1,165 @@
+block = {
+    7, 3, 1, 6, 7, 1, 7, 6, 5, 3, 1, 3, 3, 0, 6, 2, 4, 9, 1, 9, 2, 2, 5, 1, 1, 9, 6, 7, 4, 4, 2, 6, 5, 7, 4, 7, 4, 2, 3, 5, 5, 3, 4, 9, 1, 9, 4, 9, 3, 4, 
+    9, 6, 9, 8, 3, 5, 2, 0, 3, 1, 2, 7, 7, 4, 5, 0, 6, 3, 2, 6, 2, 3, 9, 5, 7, 8, 3, 1, 8, 0, 1, 6, 9, 8, 4, 8, 0, 1, 8, 6, 9, 4, 7, 8, 8, 5, 1, 8, 4, 3, 
+    8, 5, 8, 6, 1, 5, 6, 0, 7, 8, 9, 1, 1, 2, 9, 4, 9, 4, 9, 5, 4, 5, 9, 5, 0, 1, 7, 3, 7, 9, 5, 8, 3, 3, 1, 9, 5, 2, 8, 5, 3, 2, 0, 8, 8, 0, 5, 5, 1, 1, 
+    1, 2, 5, 4, 0, 6, 9, 8, 7, 4, 7, 1, 5, 8, 5, 2, 3, 8, 6, 3, 0, 5, 0, 7, 1, 5, 6, 9, 3, 2, 9, 0, 9, 6, 3, 2, 9, 5, 2, 2, 7, 4, 4, 3, 0, 4, 3, 5, 5, 7, 
+    6, 6, 8, 9, 6, 6, 4, 8, 9, 5, 0, 4, 4, 5, 2, 4, 4, 5, 2, 3, 1, 6, 1, 7, 3, 1, 8, 5, 6, 4, 0, 3, 0, 9, 8, 7, 1, 1, 1, 2, 1, 7, 2, 2, 3, 8, 3, 1, 1, 3, 
+    6, 2, 2, 2, 9, 8, 9, 3, 4, 2, 3, 3, 8, 0, 3, 0, 8, 1, 3, 5, 3, 3, 6, 2, 7, 6, 6, 1, 4, 2, 8, 2, 8, 0, 6, 4, 4, 4, 4, 8, 6, 6, 4, 5, 2, 3, 8, 7, 4, 9, 
+    3, 0, 3, 5, 8, 9, 0, 7, 2, 9, 6, 2, 9, 0, 4, 9, 1, 5, 6, 0, 4, 4, 0, 7, 7, 2, 3, 9, 0, 7, 1, 3, 8, 1, 0, 5, 1, 5, 8, 5, 9, 3, 0, 7, 9, 6, 0, 8, 6, 6, 
+    7, 0, 1, 7, 2, 4, 2, 7, 1, 2, 1, 8, 8, 3, 9, 9, 8, 7, 9, 7, 9, 0, 8, 7, 9, 2, 2, 7, 4, 9, 2, 1, 9, 0, 1, 6, 9, 9, 7, 2, 0, 8, 8, 8, 0, 9, 3, 7, 7, 6, 
+    6, 5, 7, 2, 7, 3, 3, 3, 0, 0, 1, 0, 5, 3, 3, 6, 7, 8, 8, 1, 2, 2, 0, 2, 3, 5, 4, 2, 1, 8, 0, 9, 7, 5, 1, 2, 5, 4, 5, 4, 0, 5, 9, 4, 7, 5, 2, 2, 4, 3, 
+    5, 2, 5, 8, 4, 9, 0, 7, 7, 1, 1, 6, 7, 0, 5, 5, 6, 0, 1, 3, 6, 0, 4, 8, 3, 9, 5, 8, 6, 4, 4, 6, 7, 0, 6, 3, 2, 4, 4, 1, 5, 7, 2, 2, 1, 5, 5, 3, 9, 7, 
+    5, 3, 6, 9, 7, 8, 1, 7, 9, 7, 7, 8, 4, 6, 1, 7, 4, 0, 6, 4, 9, 5, 5, 1, 4, 9, 2, 9, 0, 8, 6, 2, 5, 6, 9, 3, 2, 1, 9, 7, 8, 4, 6, 8, 6, 2, 2, 4, 8, 2, 
+    8, 3, 9, 7, 2, 2, 4, 1, 3, 7, 5, 6, 5, 7, 0, 5, 6, 0, 5, 7, 4, 9, 0, 2, 6, 1, 4, 0, 7, 9, 7, 2, 9, 6, 8, 6, 5, 2, 4, 1, 4, 5, 3, 5, 1, 0, 0, 4, 7, 4, 
+    8, 2, 1, 6, 6, 3, 7, 0, 4, 8, 4, 4, 0, 3, 1, 9, 9, 8, 9, 0, 0, 0, 8, 8, 9, 5, 2, 4, 3, 4, 5, 0, 6, 5, 8, 5, 4, 1, 2, 2, 7, 5, 8, 8, 6, 6, 6, 8, 8, 1, 
+    1, 6, 4, 2, 7, 1, 7, 1, 4, 7, 9, 9, 2, 4, 4, 4, 2, 9, 2, 8, 2, 3, 0, 8, 6, 3, 4, 6, 5, 6, 7, 4, 8, 1, 3, 9, 1, 9, 1, 2, 3, 1, 6, 2, 8, 2, 4, 5, 8, 6, 
+    1, 7, 8, 6, 6, 4, 5, 8, 3, 5, 9, 1, 2, 4, 5, 6, 6, 5, 2, 9, 4, 7, 6, 5, 4, 5, 6, 8, 2, 8, 4, 8, 9, 1, 2, 8, 8, 3, 1, 4, 2, 6, 0, 7, 6, 9, 0, 0, 4, 2, 
+    2, 4, 2, 1, 9, 0, 2, 2, 6, 7, 1, 0, 5, 5, 6, 2, 6, 3, 2, 1, 1, 1, 1, 1, 0, 9, 3, 7, 0, 5, 4, 4, 2, 1, 7, 5, 0, 6, 9, 4, 1, 6, 5, 8, 9, 6, 0, 4, 0, 8, 
+    0, 7, 1, 9, 8, 4, 0, 3, 8, 5, 0, 9, 6, 2, 4, 5, 5, 4, 4, 4, 3, 6, 2, 9, 8, 1, 2, 3, 0, 9, 8, 7, 8, 7, 9, 9, 2, 7, 2, 4, 4, 2, 8, 4, 9, 0, 9, 1, 8, 8, 
+    8, 4, 5, 8, 0, 1, 5, 6, 1, 6, 6, 0, 9, 7, 9, 1, 9, 1, 3, 3, 8, 7, 5, 4, 9, 9, 2, 0, 0, 5, 2, 4, 0, 6, 3, 6, 8, 9, 9, 1, 2, 5, 6, 0, 7, 1, 7, 6, 0, 6, 
+    0, 5, 8, 8, 6, 1, 1, 6, 4, 6, 7, 1, 0, 9, 4, 0, 5, 0, 7, 7, 5, 4, 1, 0, 0, 2, 2, 5, 6, 9, 8, 3, 1, 5, 5, 2, 0, 0, 0, 5, 5, 9, 3, 5, 7, 2, 9, 7, 2, 5, 
+    7, 1, 6, 3, 6, 2, 6, 9, 5, 6, 1, 8, 8, 2, 6, 7, 0, 4, 2, 8, 2, 5, 2, 4, 8, 3, 6, 0, 0, 8, 2, 3, 2, 5, 7, 5, 3, 0, 4, 2, 0, 7, 5, 2, 9, 6, 3, 4, 5, 0, 
+}
+
+-- unnecessary approach
+-- local sum_map = {}
+-- local buffer = {} -- store the current 4 digits here
+-- buffer["size"] = 13
+
+-- function buffer.update(self, new_val)
+--     table.insert(self, new_val)
+--     if #self > self["size"] then
+--         table.remove(self, 1)
+--     end
+-- end
+
+-- function buffer.print(self)
+--     for i=1, self["size"] do
+--         print(self[i])
+--     end
+-- end
+
+-- function buffer.getVals(self)
+--     product = 1
+--     str = ""
+
+--     -- hmm looping over wouldnt work not that we are using oop methods
+--     -- for _, number in self do
+--     --     str = str .. number
+--     --     product = product * number
+--     -- end
+
+--     for i=1, self["size"] do
+--         product = product * self[i]
+--         str = str .. self[i]
+--     end
+
+--     -- we have to use this in lua if our keys are not `valid` identifiers
+--     return {["product"]=product, ["str"]=str}
+-- end
+
+-- local max_prod = 0
+-- -- shit i just had to find the biggest sum lol
+-- for index, val in pairs(block) do
+--     -- maintaining buffer
+--     buffer:update(val)
+--     -- buffer:print()
+--     -- if stat used only once and takes a lot of time !
+--     if (#buffer == buffer.size) then
+--         d_vals = buffer:getVals()
+--         product= d_vals["product"]
+--         str= d_vals["str"]
+--         print(product, str)
+
+--         if max_prod < product then
+--             max_prod = product
+--         end
+        
+--         sum_map[product] = str
+--     end
+-- end
+
+-- print(sum_map[max_prod], max_prod)
+
+
+-- another way
+
+
+local sum_map = {}
+local buffer = {} -- store the current 4 digits here
+buffer["size"] = 13
+
+function buffer.update(self, new_val)
+    table.insert(self, new_val)
+    if #self > self["size"] then
+        table.remove(self, 1)
+    end
+end
+
+function buffer.sum(self)
+    sum = 0
+    for i=1, self["size"] do
+        sum = sum + self[i]
+    end
+    return sum
+end
+
+function buffer.getProduct(self)
+    prod = 1
+    for i=1, self["size"] do
+        if self[i] == 0 then
+            return max_prod
+        end
+        prod = prod * self[i]
+    end
+    print("prod", prod)
+    return prod
+end
+
+max_sum = 0
+max_prod = 1
+
+for index, val in pairs(block) do
+    -- maintaining buffer
+    buffer:update(val)
+    -- buffer:print()
+    -- if stat used only once and takes a lot of time !
+    if (#buffer == buffer.size) then
+        sum = buffer:sum()
+        if max_sum < sum then
+            max_sum = sum
+            -- shit i didnt consider 0
+            -- 0 doesnt affect the sum a lot but the product is reduced to 0
+            -- so blindly taking sum is not an option we will have to
+            -- check if it has 0 or return the previous max_prod if 0 DONE
+            max_prod = buffer:getProduct()
+        end
+    end
+end
+
+print(max_sum, max_prod)
+
+--[[
+    prod    5000940
+    prod    4199040
+    prod    4898880
+    prod    9797760
+    prod    50803200
+    prod    71124480
+    prod    284497920
+    prod    568995840
+    prod    283435200
+    prod    566870400
+    prod    1020366720
+    prod    1632586752
+    prod    2040733440
+    prod    14108774400
+    prod    23514624000
+    93      23514624000
+
+    ________________________________________________________
+    Executed in    7.59 millis    fish           external 
+    usr time    4.03 millis  584.00 micros    3.45 millis 
+    sys time    3.69 millis  243.00 micros    3.45 millis 
+
+]]
